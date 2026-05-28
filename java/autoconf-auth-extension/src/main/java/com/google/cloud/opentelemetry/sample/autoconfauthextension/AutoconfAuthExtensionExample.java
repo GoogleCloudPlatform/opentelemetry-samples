0fc2c5bc908d498816ca3afe3360aae12eb89522
Pranav Sharma <sharmapranav@google.com>
2026-05-28 19:25:55 +0000

    Update sample files

--- java/autoconf-auth-extension/src/main/java/com/google/cloud/opentelemetry/sample/autoconfauthextension/AutoconfAuthExtensionExample.java
+++ java/autoconf-auth-extension/src/main/java/com/google/cloud/opentelemetry/sample/autoconfauthextension/AutoconfAuthExtensionExample.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.google.cloud.opentelemetry.example.autoconfauthextension;
+package com.google.cloud.opentelemetry.sample.autoconfauthextension;
 
 import io.opentelemetry.api.metrics.LongCounter;
 import io.opentelemetry.api.trace.Span;
